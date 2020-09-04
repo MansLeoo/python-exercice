988aa6e6a94b039e500c6ed186b3287c51af4205
Mans Leo <leo.mans52@gmail.com>
2020-09-04 12:12:18 +0200

    Deuxieme commit

--- python1.py
+++ python1.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
-
+from random import *
 pi=3.14
 r=float(input("Rayon : "))
 h=float(input("hauteur : "))
 resultat=(pi*(r**2)*h)/3
 print("resultat :",resultat)
+ 
+
+"____________________________________"
+gagner=0
+rep=randint(0,100)
+
+nbre=0
+while gagner !=1 :
+    userep=int(input("Quelle le nombre generer ?"))
+    if userep>rep:
+        print("Nombre trop grand !");
+        nbre=nbre+1
+    if userep<rep : 
+        print("Nombre trop petit !");
+        nbre=nbre+1
+    if userep==rep :
+        print("Gagner ! Nombre de tentative : ",nbre)
+        gagner=1
